ae1a907c1c937fa38542dece70997870011fd9fd
mmikleusevic <mikleusevicmarin@gmail.com>
2025-09-17 15:06:09 +0200

    Added gameUI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,10 +4,15 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public event Action<float> OnTimeChanged;
+    public event Action<float> OnScoreIncreased;
+    
     public static GameManager Instance { get; private set; }
 
     private int score;
     private int highScore;
+    private bool isGameOver;
+    private float timer;
     
     private void Awake()
     {
@@ -20,6 +25,14 @@ public class GameManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+
+    private void Update()
+    {
+        if (isGameOver) return;
+        
+        timer += Time.deltaTime;
+        OnTimeChanged?.Invoke(timer);
     }
 
     private void OnEnable()
@@ -40,22 +53,34 @@ public class GameManager : MonoBehaviour
         if (scene.name != "Game") return;
         
         score = 0;
+        timer = 0;
+        isGameOver = false;
+        
+        CallOnScoreIncreased();
+        
         highScore = PlayerPrefs.GetInt(GameEvents.HIGH_SCORE, 0);
     }
     
     //TODO do something with this
     private void GameOver()
     {
-        
+        isGameOver = true;
     }
 
     public void IncreaseCollectibleScore()
     {
         score++;
 
+        CallOnScoreIncreased();
+
         if (score <= highScore) return;
         
         highScore = score;
         PlayerPrefs.SetInt(GameEvents.HIGH_SCORE, highScore);
+    }
+    
+    private void CallOnScoreIncreased()
+    {
+        OnScoreIncreased?.Invoke(score);
     }
 }
